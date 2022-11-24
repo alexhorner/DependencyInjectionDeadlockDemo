e9246bcb518353acf96b06232e292c4b4a360a83
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2022-11-24 15:54:18 +0000

    Add Singleton and Transient demos

--- DeadlockDependencyInjection/ReallyBasicDeadlockDemoTests.cs
+++ DeadlockDependencyInjection/ReallyBasicDeadlockDemoTests.cs
@@ -4,10 +4,62 @@ namespace DeadlockDependencyInjection
 {
     public class ReallyBasicDeadlockDemoTests
     {
-        private IServiceProvider _services = null!;
+        private IDeadlockDemo? DeadlockDemoFactoryOperator(IServiceProvider provider)
+        {
+            DeadlockDemoFactory factory = provider.GetRequiredService<DeadlockDemoFactory>();
+
+            return factory.GetOneAsync().GetAwaiter().GetResult();
+        }
         
-        [SetUp]
-        public void Setup()
+        [Test]
+        [Order(0)]
+        public void NoDeadlockOnSingleton()
+        {
+            IServiceCollection container = new ServiceCollection();
+
+            container.AddSingleton<DeadlockDemoFactory>();
+            container.AddSingleton<IDeadlockDemoDependency, DeadlockDemoDependencyConcrete>();
+            container.AddSingleton(typeof(IDeadlockDemo), provider => DeadlockDemoFactoryOperator(provider)!);
+
+            ServiceProvider services = container.BuildServiceProvider(new ServiceProviderOptions
+            {
+                ValidateScopes = true,
+                ValidateOnBuild = true
+            });
+            
+            using IServiceScope scope = services.CreateScope();
+
+            IDeadlockDemo demo = scope.ServiceProvider.GetRequiredService<IDeadlockDemo>();
+
+            Assert.IsNotNull(demo);
+        }
+        
+        [Test]
+        [Order(1)]
+        public void NoDeadlockOnTransient()
+        {
+            IServiceCollection container = new ServiceCollection();
+
+            container.AddScoped<DeadlockDemoFactory>();
+            container.AddScoped<IDeadlockDemoDependency, DeadlockDemoDependencyConcrete>();
+            container.AddTransient(typeof(IDeadlockDemo), provider => DeadlockDemoFactoryOperator(provider)!);
+
+            ServiceProvider services = container.BuildServiceProvider(new ServiceProviderOptions
+            {
+                ValidateScopes = true,
+                ValidateOnBuild = true
+            });
+            
+            using IServiceScope scope = services.CreateScope();
+
+            IDeadlockDemo demo = scope.ServiceProvider.GetRequiredService<IDeadlockDemo>();
+
+            Assert.IsNotNull(demo);
+        }
+
+        [Test]
+        [Order(2)]
+        public void DeadlockOnScoped()
         {
             IServiceCollection container = new ServiceCollection();
 
@@ -15,24 +67,13 @@ namespace DeadlockDependencyInjection
             container.AddScoped<IDeadlockDemoDependency, DeadlockDemoDependencyConcrete>();
             container.AddScoped(typeof(IDeadlockDemo), provider => DeadlockDemoFactoryOperator(provider)!);
 
-            _services = container.BuildServiceProvider(new ServiceProviderOptions
+            ServiceProvider services = container.BuildServiceProvider(new ServiceProviderOptions
             {
                 ValidateScopes = true,
                 ValidateOnBuild = true
             });
-        }
-
-        private IDeadlockDemo? DeadlockDemoFactoryOperator(IServiceProvider provider)
-        {
-            DeadlockDemoFactory factory = provider.GetRequiredService<DeadlockDemoFactory>();
-
-            return factory.GetOneAsync().GetAwaiter().GetResult();
-        }
-
-        [Test]
-        public void Deadlock()
-        {
-            using IServiceScope scope = _services.CreateScope();
+            
+            using IServiceScope scope = services.CreateScope();
 
             IDeadlockDemo demo = scope.ServiceProvider.GetRequiredService<IDeadlockDemo>();
 
