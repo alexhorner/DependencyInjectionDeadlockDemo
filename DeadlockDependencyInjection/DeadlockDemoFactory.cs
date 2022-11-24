93c4e74cb46aa16a195b811d8869aa7d91fe4169
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2022-11-24 17:43:04 +0000

    A little more description

--- DeadlockDependencyInjection/DeadlockDemoFactory.cs
+++ DeadlockDependencyInjection/DeadlockDemoFactory.cs
@@ -13,8 +13,8 @@ namespace DeadlockDependencyInjection
 
         public async Task<IDeadlockDemo?> GetOneAsync()
         {
-            //Do something
-
+            //Do something, anything, which needs an await
+            //Our example "something" here is a delay, but in production code this was first discovered with an async HTTP request
             await Task.Delay(1000);
 
             IDeadlockDemoDependency deadlockDemoDependency = _serviceProvider.GetRequiredService<IDeadlockDemoDependency>();
